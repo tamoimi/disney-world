a657c9ba1a3bcaa5f5594aa4254e3f738adb9859
tami <tamiortami@gmail.com>
2024-04-17 10:33:51 +0900

    refactor: moment for date

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { API_URL } from "../api/disney-api";
+import moment from "moment";
 
 async function getCharacters() {
   const response = await fetch(`${API_URL}/character`);
@@ -9,14 +10,14 @@ async function getCharacters() {
 
 export default async function Home() {
   const characters = await getCharacters();
-  console.log("characters", characters);
+  console.log("characters", characters.films);
 
   return (
     <main className="p-5 lg:px-72">
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {characters &&
           characters.data.map((c: any) => (
-            <div key={c.id} className="bg-white p-5 rounded shadow">
+            <div key={c.id} className="bg-slate-50/35 p-5 rounded shadow">
               <Image
                 src={c.imageUrl}
                 width={100}
@@ -27,7 +28,9 @@ export default async function Home() {
                 className="rounded"
               />
               <h2 className="text-center mt-2 text-sm font-semibold">{c.name}</h2>
-              <h2 className="text-center mt-2 text-sm">{c.createdAt}</h2>
+              <h2 className="text-center mt-2 text-sm">
+                {moment(c.createdAt).format("MMM Do YYYY")}
+              </h2>
               <h2 className="text-center mt-2 text-sm">{c.films}</h2>
             </div>
           ))}
@@ -35,4 +38,3 @@ export default async function Home() {
     </main>
   );
 }
-// super hero api로 변경
